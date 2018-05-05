15bcccd91c60695858e5ca1e38d17e4254fbae13
Cameron Brouk <cbrouk@localhost.localdomain>
2018-05-04 23:23:08 -0500

    deleted logging on development, deleted deprecated seo functionality

--- src/app/core/logging/success.model.ts
+++
@@ -1,6 +0,0 @@
-export interface Success {
-    file: string;
-    feature: string;
-    event: string;
-    description: string;
-}
--- src/app/core/seo/seo.server.ts
+++
@@ -1,4 +0,0 @@
-import { enableProdMode } from '@angular/core';
-export { SeoServerModule } from './seo.server.module';
-
-enableProdMode();
